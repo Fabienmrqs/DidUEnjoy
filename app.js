ae9b0e0a8ae357d98950efee74ccfd56531afd8c
Marques Fabien <fabienmrqs@gmail.com>
2023-11-15 20:33:19 +0100

    final work

---
+++ app.js
@@ -0,0 +1,44 @@
+var kpi_created = document.getElementById('kpi_created');
+var cards_settings = document.getElementById('card_settings');
+var kpi_ico_info = document.getElementById('kpi_satisfaction_ico_info');
+
+
+
+kpi_created.addEventListener('click', changeBorderBlue )
+
+
+
+  function createKPI(){
+    const kpiDiv = document.createElement("div");
+    kpiDiv.id = 'kpi_created'
+     kpiDiv.classList.add("kpi_created_display_block");
+
+     kpiDiv.addEventListener('click', function(e) {
+        kpiDiv.style.border='solid blue';
+        cards_settings.style.display ="block";
+        if(document.forms['card_settings_form'].kpi_name.value === "") {
+            kpi_ico_info.style.display = "none"
+        }
+    }, false);
+    document.getElementById("kpi_container").appendChild(kpiDiv);
+    
+  }
+
+
+  function dropDown() {
+    console.log('dropdown')
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.card_settings_trend_input')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
